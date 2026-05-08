ec866c897c25a2794d33add56cfd2675b2479b94
tdanie1 <tdanie1@hotmail.com>
2026-05-08 14:17:36 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
+-- Nombre
 st.title("Mi primera aplicación Streamlit")
 nombre = st.text_input("¿Cuál es tu nombre?")
 if nombre:
     st.success(f"Hola {nombre}, ¡bienvenido a tu app web!")
 
+-- Factoriales
 def factorial(n):
     return 1 if n == 0 else n * factorial(n-1)
 
@@ -12,3 +14,14 @@ st.title("Calculadora de factoriales")
 numero = st.number_input("Ingresa un número:", min_value=0, step=1)
 if st.button("Calcular"):
     st.success(f"El factorial es {factorial(numero)}")
+
+-- Calculadora de cuadrados
+st.title("Calculadora de cuadrados")
+
+# Entrada numérica
+numero = st.number_input("Ingresa un número:", value=0)
+
+# Botón
+if st.button("Calcular"):
+    resultado = numero ** 2
+    st.success(f"El cuadrado es: {resultado}")
