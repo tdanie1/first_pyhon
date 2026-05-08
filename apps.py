90dbb1d1f0c22bcac59235011304a64b6e05e1e9
tdanie1 <tdanie1@hotmail.com>
2026-05-08 13:56:10 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.title("Mi primera aplicación Streamlit")
+nombre = st.text_input("¿Cuál es tu nombre?")
+if nombre:
+    st.success(f"Hola {nombre}, ¡bienvenido a tu app web!")
+
 def factorial(n):
     return 1 if n == 0 else n * factorial(n-1)
 
