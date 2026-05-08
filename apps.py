569147748b2dc388532ed5d13ebd35bc3a7868e5
tdanie1 <tdanie1@hotmail.com>
2026-05-08 14:20:22 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -15,7 +15,7 @@ numero = st.number_input("Ingresa un número:", min_value=0, step=1)
 if st.button("Calcular"):
     st.success(f"El factorial es {factorial(numero)}")
 
--- Calculadora de cuadrados
+
 st.title("Calculadora de cuadrados")
 
 # Entrada numérica
